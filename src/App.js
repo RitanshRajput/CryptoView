6d4edc31d8a6834080ea47b0e036b776adb3f010
Ritansh Rajput <it2042@global.org.in>
2023-02-17 14:21:05 +0530

    cryptocurrency app using chakra-ui react framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Coins from "./Components/Coins";
+import Exchanges from "./Components/Exchanges";
+import CoinDetails from "./Components/CoinDetails";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/coin/:id" element={<CoinDetails />} />
+      </Routes>
+    <Footer />
+    </Router>
   );
 }
 
 export default App;
+
+// 6:33:24
